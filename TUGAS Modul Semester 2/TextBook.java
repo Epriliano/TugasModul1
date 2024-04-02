8ceb384cbff964cadae9c45bbd13fdd226ce8818
Epriliano <eprilianodimas27@gmail.com>
2024-04-02 14:56:55 +0700

    Modul Semester 2

---
+++ TUGAS Modul Semester 2/TextBook.java
@@ -0,0 +1,8 @@
+public class TextBook extends Book{
+    private String category;
+
+    public TextBook(String bookId, String title, String author, String category, int stock){
+        super(bookId, title, author, category, stock);
+
+    }
+}
